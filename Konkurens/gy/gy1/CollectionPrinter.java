d867c25a4b34e67fc4154925c4b6b310e8a02e72
domonkos.kutas <Domonkos.Kutas@thyssenkrupp-automotive.com>
2023-09-14 11:48:44 +0200

    Add everything else

---
+++ Konkurens/gy/gy1/CollectionPrinter.java
@@ -0,0 +1,19 @@
+package Konkurens;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CollectionPrinter<T> {
+
+    public static <T> void print(java.util.Collection<T> coll) {
+        for (var elem : coll) {
+            System.out.println(elem);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> stringList = new ArrayList<>(Arrays.asList("Cordo", "Lista"));
+        print(stringList);
+    }
+}
---
+++ Konkurens/gy/gy1/Triple.java
@@ -0,0 +1,17 @@
+package Konkurens;
+
+public class Triple<T, U, V> {
+    private T first;
+    private U second;
+    private V third;
+
+    @Override
+    public boolean equals(Object that) {
+        if (that instanceof Triple<?, ?, ?>) {
+            Triple<?, ?, ?> thatTriple = (Triple<?, ?, ?>) that;
+        }
+        return false;
+
+    }
+
+}
